f9c11068981c38a7174ecb88235b600550af1b49
ValerieHMoore <periwinkle17@outlook.com>
2018-11-29 09:29:53 -0500

    first draft of sandwiches index view

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -1,7 +1,7 @@
 class Sandwich < ApplicationRecord
     belongs_to :user
     has_many :fillings, through: :sandwich_fillings
-    accepts_nested_attributes_for :sandwich_fillings
+    # accepts_nested_attributes_for :quantity
 
     validates :sandwich_name, presence: true
     validates :bread_name, presence: true
