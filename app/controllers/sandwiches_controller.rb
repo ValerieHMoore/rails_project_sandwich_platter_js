9a69679456e5486c4b8de06e16a94d1273576afc
ValerieHMoore <periwinkle17@outlook.com>
2018-11-29 08:37:53 -0500

    first draft of sandwiches_controller

--- app/controllers/sandwiches_controller.rb
+++ app/controllers/sandwiches_controller.rb
@@ -1,2 +1,37 @@
 class SandwichesController < ApplicationController
+
+    def new
+        @sandwich = Sandwich.new
+    end
+
+    def create
+        @sandwich = Sandwich.create(sandwich_params)
+        redirect_to sandwich_path(@sandwich)
+    end
+
+    def index
+        @sandwiches = Sandwich.all
+    end
+
+    def show
+        @sandwich = Sandwich.find_by(id: params[:id])
+    end
+
+    def edit
+        @sandwich = Sandwich.find_by(id: params[:id])
+    end
+
+    def update
+        @sandwich = Sandwich.find_by(id: params[:id])
+        @sandwich.update(sandwich_params)
+        redirect_to sandwich_path(@sandwich)
+    end
+
+    private
+
+    def sandwich_params
+        params.require(:sandwich).permit(:name, :id, :toast, :grill, :open_face, filling_attributes:[:name], sandwich_filling_attributes:[:quantity])
+    end
+    
 end
+
