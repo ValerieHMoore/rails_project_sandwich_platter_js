71e3e160a39e8de608a62a0bdf1b246dd59db87a
ValerieHMoore <periwinkle17@outlook.com>
2018-11-30 14:43:10 -0500

    remove show from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-    before_action :require_login, only: [:show]
     
     def new
         @user = User.new
@@ -9,20 +8,8 @@ class UsersController < ApplicationController
         @user = User.create(user_params)
         if @user
             session[:user_id] = @user.id
-            redirect_to user_path(@user)
+            redirect_to sandwiches_path
         else
-        end
-    end
-
-    def show
-        @user = User.find_by(:id => params[:id])
-        if @user.nil?
-          flash[:notice] = "User not found."
-          redirect_to users_path
-        else
-          @user_message = params[:user_message] if params[:user_message]
-          @user_message ||= false
-          render :show
         end
     end
 
@@ -31,6 +18,5 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:email, :password_digest)
     end
-    
-end
 
+end
