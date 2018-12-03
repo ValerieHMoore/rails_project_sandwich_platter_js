20567512e9daac981332c3c3a46fe45409fd8436
ValerieHMoore <periwinkle17@outlook.com>
2018-12-03 15:18:46 -0500

    update spec.md

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,24 +3,20 @@ class SessionsController < ApplicationController
     def new
     end
           
-    # def create
-    #   @user = User.find_or_create_by(uid: auth['uid']) do |u|
-    #     u.name = auth['info']['name']
-    #     u.email = auth['info']['email']
-    #     u.image = auth['info']['image']
-    #   end
-   
-    #   session[:user_id] = @user.id
-   
-    #   render 'welcome/home'
-    # end
+    def create
+      @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        u.email = auth['info']['email']
+        u.image = auth['info']['image']
+      end
+      session[:user_id] = @user.id
+      render 'welcome/home'
+    end
     
     def create
       @user = User.find_by(email: params[:session][:email].downcase)
       if @user && @user.authenticate(params[:session][:password])
         session[:user_id] = @user.id 
         redirect_to sandwiches_path(@user)
-        # binding.pry
       else
         render :new
       end
