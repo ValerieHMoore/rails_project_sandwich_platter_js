a9356d94d542b976d40ed8c2ea268d3524fc8a18
ValerieHMoore <periwinkle17@outlook.com>
2018-12-04 16:51:55 -0500

    add human_boolean method to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,19 @@
 module ApplicationHelper
-    def require_login
-        redirect_to root_path unless logged_in?
-      end
+
+  def human_boolean(boolean)
+    boolean ? "yes" : "no"
+  end
+
+  def require_login
+    redirect_to root_path unless logged_in?
+  end
     
-      def logged_in?
-        !!current_user
-      end
+  def logged_in?
+    !!current_user
+  end
     
-      def current_user
-        @_current_user ||= User.find_by(:id => session[:user_id])
-      end
+  def current_user
+    @_current_user ||= User.find_by(:id => session[:user_id])
+  end
+
 end
