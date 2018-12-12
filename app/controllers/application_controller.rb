73915e51224a6328126fef41bac1a0f0318f6bcf
ValerieHMoore <periwinkle17@outlook.com>
2018-12-12 18:05:50 -0500

    update sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,8 @@ class ApplicationController < ActionController::Base
 
   helper_method :logged_in?
 
-    # protect_from_forgery with: :exception
-
-    helper_method :current_user
+  helper_method :current_user
     
-    # def current_user
-    #  @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    # end
-  
     def current_user
       #for redirects
       if session[:user_id]
