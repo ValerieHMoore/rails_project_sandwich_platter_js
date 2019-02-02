a33e2580fae75bb0955095d7fa86ca9556955c46
ValerieHMoore <periwinkle17@outlook.com>
2019-02-01 21:42:49 -0500

    create sandwiches.js

---
+++ app/assets/javascripts/sandwiches.js
@@ -0,0 +1,11 @@
+class Sandwich {
+	constructor(obj) {
+		this.id = obj.id
+		this.sandwich_name = obj.sandwich_name
+        this.bread_name = obj.bread_name
+        this.grill = obj.grill
+        this.open_face = obj.open_face
+        this.filling_name = obj.filling_name
+        this.quantity = obj.quantity
+    }
+}
